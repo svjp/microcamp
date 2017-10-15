e5d434ea40dd805f7021344cb5715bfc369e3a3e
João Paulo Vieira da Silva <svjp.1999@gmail.com>
2017-10-15 21:45:29 -0200

    Finalizar validacao do cpf utilizando expressao regular

--- javascript/aula-08/regexp.js
+++ javascript/aula-08/regexp.js
@@ -1,21 +1,23 @@
 /** Utilizando Expressões Regulares para validar CEP */
 var validacaoCEP = function validacaoCEP(value) {
-	var criterios = new RegExp(/^[0-9]{5}\-[0-9]{3}$/),
-		output = criterios.test(value);
-
-	console.log(output); // 00000-000
+	var criterios = new RegExp(/^[0-9]{5}\-[0-9]{3}$/);
+	return criterios.test(value);
 };
 
 /** Utilizando Expressões Regulares para validar CEP */
 var validacaoCPF = function validacaoCPF(value) {
-	var criterios = new RegExp(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/),
-		output = criterios.test(value);
-
-	console.log(output); // 000.000.000-00
+	var criterios = new RegExp(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/);
+	return criterios.test(value);
 };
 
 /** Acionando um evento no botão #enviar para ativar ambas funções validacaoCPF() e validacaoCEP() */
 document.querySelector('#enviar').addEventListener('click', function() {
-	var cep = document.querySelector('input[name="cep"]');
-	validacaoCEP(cep.value);
+	var cep = document.querySelector('input[name="cep"]').value,
+		cpf = document.querySelector('input[name="cpf"]').value;
+
+	if (validacaoCPF(cpf) && validacaoCEP(cep)) {
+		console.log('CEP e CPF Válidos');
+	} else {
+		console.log('CEP e CPF Inválidos');
+	}
 });
