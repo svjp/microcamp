07f58335aed6bd246be8418d8ae46380a078b323
João Paulo Vieira da Silva <svjp.1999@gmail.com>
2017-10-23 17:47:14 -0200

    Comentar a validacao do formulario com uma breve explicacao do codigo

--- javascript/aula-10/validation.js
+++ javascript/aula-10/validation.js
@@ -2,10 +2,10 @@
 document.querySelector('#cadastro').addEventListener('submit', function(event) {
 	var input = document.querySelectorAll('input'),
 	    criterios = {
-			'nome'  : new RegExp(/^[A-Za-zà-ú]{1,}$/),
-			'email' : new RegExp(/^[a-z0-9]{4,}\@[a-z]{4,}\.[a-z]{3,}(\.[a-z]{2,})?$/),
-			'data'  : new RegExp(/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/),
-			'cnpj'  : new RegExp(/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}\-[0-9]{2}$/)
+			'nome': new RegExp(/^[A-Za-zà-ú]{1,}$/),
+			'email': new RegExp(/^[a-z0-9]{4,}\@[a-z]{4,}\.[a-z]{3,}(\.[a-z]{2,})?$/),
+			'data': new RegExp(/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/),
+			'cnpj': new RegExp(/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}\-[0-9]{2}$/)
 		};
 
 
@@ -31,8 +31,20 @@ document.querySelector('#cadastro').addEventListener('submit', function(event) {
 		}
 	}
 
-	/** Validação do campo nome */
-	// if (!isNaN(input[0].value) || input[0].value == '') {
+	if (event.defaultPrevented != true) {
+		alert('Formulário enviado com sucesso!');
+	}
+
+	/**
+	 * A validação abaixo seria a versão mais longa e simples da que utilizamos
+	 * o operador de loop, simplificamos e tratamos este script evitando a repe-
+	 * tição desnecessária de código.
+	 */
+
+	/********************
+	 * Nome: João Paulo *
+	 ********************/
+	// if (!isNaN(input[0].value) && /^[à-ú]$/.test(input[0].value) || input[0].value == '') {
 	// 	alert('Por favor insira um ' + input[0].name + ' válido');
 	// 	input[i].style.border = '1px solid red';
 	// 	event.preventDefault();
@@ -40,7 +52,9 @@ document.querySelector('#cadastro').addEventListener('submit', function(event) {
 	// 	input[1].style.borderColor = '#a9a9a9';
 	// }
 
-	/** Validação do campo e-mail */
+	/*********************************
+	 * E-mail: meuemail22@server.com *
+	 *********************************/
 	// if (/^[a-z]{4,}\@[a-z]{4,}\.[a-z]{3,}(\.[a-z]{2,})?$/.test(input[1].value) === false || input[1].value == '') {
 	// 	alert('Por favor insira um ' + input[1].name + ' válido');
 	// 	input[1].style.border = '1px solid red';
@@ -49,7 +63,9 @@ document.querySelector('#cadastro').addEventListener('submit', function(event) {
 	// 	input[1].style.borderColor = '#a9a9a9';
 	// }
 
-	// /** Validação do campo data */
+	/****************************
+	 * Data: MM/DD/YYYY         *
+	 ****************************/
 	// if (/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/.test(input[2].value) === false || input[2].value == '') {
 	// 	alert('Por favor insira uma ' + input[2].name + ' válido');
 	// 	input[2].style.border = '1px solid red';
@@ -58,7 +74,9 @@ document.querySelector('#cadastro').addEventListener('submit', function(event) {
 	// 	input[2].style.borderColor = '#a9a9a9';
 	// }
 
-	// /** Validação do campo CNPJ */
+	/****************************
+	 * CNPJ: XX.XXX.XXX/XXXX-XX *
+	 ****************************/
 	// if (/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}\-[0-9]{2}$/.test(input[3].value) === false || input[3].value == '') {
 	// 	alert('Por favor insira uma ' + input[3].name + ' válido');
 	// 	input[3].style.border = '1px solid red';
@@ -66,9 +84,4 @@ document.querySelector('#cadastro').addEventListener('submit', function(event) {
 	// } else {
 	// 	input[3].style.borderColor = '#a9a9a9';
 	// }
-
-	/** Verifica se todos os campos são válidos, se sim joga um alert */
-	if (event.defaultPrevented != true) {
-		alert('Formulário enviado com sucesso!');
-	}
 });
