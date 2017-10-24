4323f68f0ab4c6e98eac63ceff9744632e2b7e56
João Paulo Vieira da Silva <svjp.1999@gmail.com>
2017-10-24 11:56:48 -0200

    Converter todos os arquivos do curso de Tabs para Espaços
    O motivo de fazer essa conversão é puramente para que o código
    seja bem apresentado no preview do GitHub. As configurações na mi-
    nha máquina estão setadas para converter Tabs em Spaces.

--- javascript/aula-08/regexp.js
+++ javascript/aula-08/regexp.js
@@ -1,23 +1,23 @@
 /** Utilizando Expressões Regulares para validar CEP */
 var validacaoCEP = function validacaoCEP(value) {
-	var criterios = new RegExp(/^[0-9]{5}\-[0-9]{3}$/);
-	return criterios.test(value);
+  var criterios = new RegExp(/^[0-9]{5}\-[0-9]{3}$/);
+  return criterios.test(value);
 };
 
 /** Utilizando Expressões Regulares para validar CEP */
 var validacaoCPF = function validacaoCPF(value) {
-	var criterios = new RegExp(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/);
-	return criterios.test(value);
+  var criterios = new RegExp(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/);
+  return criterios.test(value);
 };
 
 /** Acionando um evento no botão #enviar para ativar ambas funções validacaoCPF() e validacaoCEP() */
 document.querySelector('#enviar').addEventListener('click', function() {
-	var cep = document.querySelector('input[name="cep"]').value,
-		cpf = document.querySelector('input[name="cpf"]').value;
+  var cep = document.querySelector('input[name="cep"]').value,
+    cpf = document.querySelector('input[name="cpf"]').value;
 
-	if (validacaoCPF(cpf) && validacaoCEP(cep)) {
-		console.log('CEP e CPF Válidos');
-	} else {
-		console.log('CEP e CPF Inválidos');
-	}
+  if (validacaoCPF(cpf) && validacaoCEP(cep)) {
+    console.log('CEP e CPF Válidos');
+  } else {
+    console.log('CEP e CPF Inválidos');
+  }
 });
